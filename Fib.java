623cf7c9d79f077bace05288f270ba4d452f7cdf
Akash Das <akashdas.ny@gmail.com>
2019-05-13 18:49:29 -0400

    add and implement methods

--- Fib.java
+++ Fib.java
@@ -5,14 +5,18 @@
   
   Use type "long" instead of "int", because rabbits.
  */
+import java.lang.Math;
 
 public class Fib {
     /** 
       @return the nth Fibonacci number
               by implementing the recurrence relation
      */
-    public ?? long fib_recurrence( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_recurrence( int n) {
+	if (n <= 2) return 1;
+	else {
+	    return fib_recurrence(n-1) + fib_recurrence(n-2);
+	}
     }
      /* These are class / instance methods because...
     */
@@ -22,8 +26,18 @@ public class Fib {
       @return the nth Fibonacci number
               calculated via the 8th-grade algorithm
      */
-    public ?? long fib_grade8( int n) {
-        return -2; // invalid Fibonacci number
+    public static long fib_grade8( int n) {
+	int current = 1;
+	int prev = 0;
+	int count = 1;
+	while (count < n) {
+	    int tempPrev = prev;
+	    int tempCurrent = current;
+	    current += tempPrev;
+	    prev = tempCurrent;
+	    count++;
+	}
+        return current; // invalid Fibonacci number
     }
     /* Time complexity:
        Consider the size of the problem to be ...
@@ -43,8 +57,11 @@ public class Fib {
               Type is double so that this exercise can
               ignore rounding issues.
      */
-    public ?? double fib_Binet( int n) {
-        return -2; // invalid Fibonacci number
+    public static double fib_Binet( int n) {
+	double phi = (1 + Math.sqrt(5)) / 2;
+	double psi = (1 - Math.sqrt(5)) / 2;
+	
+        return Math.sqrt(0.2) * (Math.pow(phi,n) - Math.pow(psi,n)); // invalid Fibonacci number
     }
     /* Time complexity:
        Consider the size of the problem to be ...
